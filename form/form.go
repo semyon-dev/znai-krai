226890e93002d1ac54a852403c9d3760887b034a
semyon-dev <semyon-dev@protonmail.com>
2020-06-19 21:15:36 +0300

    formQuestions improved

--- form/form.go
+++ form/form.go
@@ -43,7 +43,6 @@ func Questions(c *gin.Context) {
 		"status",
 		"Какой ваш статус?",
 		true,
-		"",
 		valuesStatusOther,
 	)
 
@@ -51,7 +50,6 @@ func Questions(c *gin.Context) {
 		"salary_of_prisoners",
 		"Укажите, в каком диапазоне находится месячная зарплата заключенных?",
 		false,
-		"",
 		model.ViolationsSalaryTypes,
 	)
 
@@ -59,7 +57,6 @@ func Questions(c *gin.Context) {
 		"violations_of_food_sure",
 		"Были ли нарушения, связанные с питанием?",
 		false,
-		"",
 		valuesYesNoDifficult,
 	)
 
@@ -75,7 +72,6 @@ func Questions(c *gin.Context) {
 		"violations_of_medical_care_sure",
 		"Были ли нарушения, связанные с оказанием медицинской помощи?",
 		false,
-		"",
 		valuesYesNoDifficult,
 	)
 
@@ -87,20 +83,34 @@ func Questions(c *gin.Context) {
 		model.ViolationsMedicalCareTypes,
 	)
 
+	data.newQuestionChooseOne(
+		"provide_name_and_contacts_sure",
+		"Готовы ли вы сообщить свое имя и контакты?",
+		false,
+		valuesYesNo,
+	)
+
 	data.newQuestionTextfield(
 		"provide_name_and_contacts",
-		"Готовы ли вы сообщить свое имя и контакты? Если нет - пропустите вопрос.",
+		"Ваши контакты:",
 		false,
 		"Мамонтов Власий Демьянович, 89001112233",
-		"",
+		"provide_name_and_contacts_sure",
 		nil,
+	)
+
+	data.newQuestionChooseOne(
+		"physical_impact_sure",
+		"Сталкивались ли вы с применением физического воздействия?",
+		false,
+		valuesYesNoDifficult,
 	)
 
 	data.newQuestionMultiply(
 		"physical_impact_from_employees",
 		"С какими фактами применения физического воздействия со стороны сотрудников ФСИН вам приходилось сталкиваться?",
 		false,
-		"",
+		"physical_impact_sure",
 		model.ViolationsPhysicalImpactTypes,
 	)
 
@@ -108,70 +118,120 @@ func Questions(c *gin.Context) {
 		"physical_impact_from_prisoners",
 		"С какими фактами применения физического воздействия со стороны заключенных вам приходилось сталкиваться?",
 		false,
-		"",
+		"physical_impact_sure",
 		model.ViolationsPhysicalImpactTypes,
 	)
 
-	data.newQuestionMultiply(
-		"visits_with_relatives",
-		"Какие нарушения, связанные с предоставлением свиданий с Родственниками, вам известны?",
+	data.newQuestionChooseOne(
+		"communication_with_lawyer_sure",
+		"Сталкивались ли вы с нарушениями связанные с общением с адвокатом",
 		false,
-		"",
-		model.ViolationsVisitsWithRelatives,
-	)
-
-	data.newQuestionMultiply(
-		"violations_of_clothes",
-		"Какие нарушения, связанные с одеждой, вы можете отметить?",
-		false,
-		"",
-		model.ViolationsClothes,
-	)
-
-	data.newQuestionMultiply(
-		"violations_staging",
-		"Какие нарушения, связанные с этапированием заключенных вам известны?",
-		false,
-		"",
-		model.ViolationsClothes,
-	)
-
-	data.newQuestionMultiply(
-		"violations_penalties_related_to_placement",
-		"С какими нарушениями при применении мер взыскания, связанных с водворением в карцер и ШИЗО, переводом в ПКТ, ЕПКТ и на СУС, вам приходилось сталкиваться?",
-		false,
-		"",
-		model.ViolationsWithPlacementInPunishmentCellTypes,
+		valuesYesNoDifficult,
 	)
 
 	data.newQuestionMultiply(
 		"communication_with_lawyer",
 		"Какие нарушения, связанные с общением с адвокатом (иным лицом, имеющим право на оказание юридической помощи), вам известны?",
 		false,
-		"",
+		"communication_with_lawyer_sure",
 		model.ViolationsCommunicationWithOthers,
+	)
+
+	data.newQuestionChooseOne(
+		"visits_with_relatives_sure",
+		"Сталкивались ли вы с нарушениями с предоставлением свиданий с родственниками?",
+		false,
+		valuesYesNoDifficult,
+	)
+
+	data.newQuestionMultiply(
+		"visits_with_relatives",
+		"Какие нарушения, связанные с предоставлением свиданий с родственниками, вам известны?",
+		false,
+		"visits_with_relatives_sure",
+		model.ViolationsVisitsWithRelatives,
+	)
+
+	data.newQuestionChooseOne(
+		"violations_of_clothes_sure",
+		"Сталкивались ли вы с нарушениями связанные с одеждой?",
+		false,
+		valuesYesNoDifficult,
+	)
+
+	data.newQuestionMultiply(
+		"violations_of_clothes",
+		"Какие нарушения, связанные с одеждой, вы можете отметить?",
+		false,
+		"violations_of_clothes_sure",
+		model.ViolationsClothes,
+	)
+
+	data.newQuestionChooseOne(
+		"violations_staging_sure",
+		"Сталкивались ли вы с нарушениями связанные с этапированием заключенных?",
+		false,
+		valuesYesNoDifficult,
+	)
+
+	data.newQuestionMultiply(
+		"violations_staging",
+		"Какие нарушения, связанные с этапированием заключенных вам известны?",
+		false,
+		"violations_staging_sure",
+		model.ViolationsClothes,
+	)
+
+	data.newQuestionChooseOne(
+		"violations_penalties_related_to_placement_sure",
+		"Сталкивались ли вы с нарушениями при применении мер взыскания, связанных с водворением в карцер и ШИЗО, переводом в ПКТ, ЕПКТ и на СУС?",
+		false,
+		valuesYesNoDifficult,
+	)
+
+	data.newQuestionMultiply(
+		"violations_penalties_related_to_placement",
+		"С какими нарушениями при применении мер взыскания, связанных с водворением в карцер и ШИЗО, переводом в ПКТ, ЕПКТ и на СУС, вам приходилось сталкиваться?",
+		false,
+		"violations_penalties_related_to_placement_sure",
+		model.ViolationsWithPlacementInPunishmentCellTypes,
+	)
+
+	data.newQuestionChooseOne(
+		"violations_religious_rites_sure",
+		"Сталкивались ли вы с нарушениями связанные с религией?",
+		false,
+		valuesYesNoDifficult,
 	)
 
 	data.newQuestionMultiply(
 		"violations_religious_rites_from_employees",
 		"С какими запретами (нарушениями) на отправление религиозных обрядов со стороны сотрудников ФСИН вам приходилось сталкиваться?",
 		false,
-		"",
+		"violations_religious_rites_sure",
 		model.ViolationsReligiousViolations,
 	)
+
 	data.newQuestionMultiply(
 		"violations_religious_rites_from_prisoners",
 		"С какими запретами (нарушениями) на отправление религиозных обрядов со стороны заключенных вам приходилось сталкиваться?",
 		false,
-		"",
+		"violations_religious_rites_sure",
 		model.ViolationsReligiousViolations,
+	)
+
+	data.newQuestionChooseOne(
+		"psychological_impact_from_employees_sure",
+		"Сталкивались ли вы с нарушениями связанные с применением психологического воздействия?",
+		false,
+		valuesYesNoDifficult,
 	)
 
 	data.newQuestionMultiply(
 		"psychological_impact_from_employees",
 		"С какими фактами применения психологического воздействия со стороны сотрудников ФСИН вам приходилось сталкиваться?",
 		false,
-		"",
+		"psychological_impact_from_employees_sure",
 		model.ViolationsPsychologicalImpact,
 	)
 
@@ -179,15 +239,22 @@ func Questions(c *gin.Context) {
 		"psychological_impact_from_prisoners",
 		"С какими фактами применения психологического воздействия со стороны заключенных вам приходилось сталкиваться?",
 		false,
-		"",
+		"psychological_impact_from_employees_sure",
 		model.ViolationsPsychologicalImpact,
+	)
+
+	data.newQuestionChooseOne(
+		"extortions_from_employees_sure",
+		"Сталкивались ли вы с нарушениями с фактами вымогательства со стороны сотрудников ФСИН?",
+		false,
+		valuesYesNoDifficult,
 	)
 
 	data.newQuestionMultiply(
 		"extortions_from_employees",
 		"В каких случаях вы сталкивались с фактами вымогательства со стороны сотрудников ФСИН?",
 		false,
-		"",
+		"extortions_from_employees_sure",
 		model.ViolationsExtortionsFromEmployeesTypes,
 	)
 
@@ -195,7 +262,6 @@ func Questions(c *gin.Context) {
 		"corruption_from_employees",
 		"Приходилось ли вам сталкиваться с иными случаями коррупции сотрудников ФСИН?",
 		false,
-		"",
 		valuesYesNoDifficultOther,
 	)
 
@@ -203,7 +269,6 @@ func Questions(c *gin.Context) {
 		"extortions_from_prisoners",
 		"Приходилось ли вам сталкиваться с фактами вымогательства со стороны заключенных?",
 		false,
-		"",
 		valuesYesNoDifficultOther,
 	)
 
@@ -211,7 +276,6 @@ func Questions(c *gin.Context) {
 		"can_prisoners_submit_complaints",
 		"Есть ли у заключенных возможность направлять жалобы, ходатайства и заявления в надзирающие органы и правозащитные организации?",
 		false,
-		"",
 		valuesYesNoDifficultOther,
 	)
 
@@ -219,7 +283,6 @@ func Questions(c *gin.Context) {
 		"labor_slavery",
 		"Известны ли вам случаи трудового рабства?",
 		false,
-		"",
 		valuesYesNoDifficultOther,
 	)
 
@@ -227,7 +290,6 @@ func Questions(c *gin.Context) {
 		"help_european_court",
 		"Мы могли бы помочь вам в составлении жалобы в Европейский суд по поводу нарушений в местах лишения свободы. Хотели бы вы получить такую помощь?",
 		false,
-		"",
 		valuesYesNoOther,
 	)
 
@@ -235,7 +297,6 @@ func Questions(c *gin.Context) {
 		"public_disclosure",
 		"Согласны ли Вы на публичную огласку приведенных вами фактов?",
 		true,
-		"",
 		valuesYesNo,
 	)
 
@@ -243,7 +304,6 @@ func Questions(c *gin.Context) {
 		"processing_personal_data",
 		"Согласны ли вы на обработку Ваших персональных данных?",
 		true,
-		"",
 		valuesYes,
 	)
 
@@ -251,8 +311,6 @@ func Questions(c *gin.Context) {
 		"additional_information",
 		"Если ли у вас есть дополнительная информация, которой вы готовы поделиться с нами, то ее можно написать здесь:",
 		false,
-		"",
-		"",
 		nil,
 	)
 
@@ -283,16 +341,16 @@ func (data *QuestionsData) newQuestionMultiply(name, questionName string, requir
 	data.newQuestion(name, questionName, "choose_multiply", required, "", requires, values, "")
 }
 
-func (data *QuestionsData) newQuestionChooseOne(name, questionName string, required bool, requires string, values []string) {
-	data.newQuestion(name, questionName, "choose_one", required, "", requires, values, "")
+func (data *QuestionsData) newQuestionChooseOne(name, questionName string, required bool, values []string) {
+	data.newQuestion(name, questionName, "choose_one", required, "", "", values, "")
 }
 
 func (data *QuestionsData) newQuestionTextfield(name, questionName string, required bool, hint, requires string, values []string) {
 	data.newQuestion(name, questionName, "textfield", required, hint, requires, values, "")
 }
 
-func (data *QuestionsData) newQuestionTextarea(name, questionName string, required bool, hint, requires string, values []string) {
-	data.newQuestion(name, questionName, "textarea", required, hint, requires, values, "")
+func (data *QuestionsData) newQuestionTextarea(name, questionName string, required bool, values []string) {
+	data.newQuestion(name, questionName, "textarea", required, "", "", values, "")
 }
 
 // for upload files
